a480b122662f9143eaaa1859dfb9fe84e5645ac0
Etienne <etiennebergeron91@gmail.com>
2017-02-26 16:17:03 -0500

    Lecture du fichier d'operations (corrections)

--- fileSystem.c
+++ fileSystem.c
@@ -1,6 +1,9 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include "fileSystem.h"
 
 int main(int argc, char *argv[]){
     // Fichier d'operations donne en parametre
@@ -16,26 +19,20 @@ int main(int argc, char *argv[]){
     ftruncate(fileno(disque), 512 * 1000);
 
     // Lecture des operations
-    while(fscanf(operations, "%s", operation) != EOF){
-        switch(operation){
-            case "creation_fichier\0":
-                creationFicher();
-                break;
-            case "suppression_fichier\0":
-                suppressionFichier();
-                break;
-            case "creation_repertoire\0":
-                creationRepertoire();
-                break;
-            case "suppression_repertoire\0":
-                suppressionRepertoire();
-                break;
-            case "lire_fichier\0":
-                lireFichier();
-                break;
-            default:
-                fprintf(stderr, "\n" );
-                return EXIT_FAILURE;
+    while(fscanf(operations, "%s", operation) != EOF){        
+        if(strcmp(operation, "creation_fichier\0")){
+            creationFicher();
+        } else if(strcmp(operation, "suppression_fichier\0")){
+            suppressionFichier();
+        } else if(strcmp(operation, "creation_repertoire\0")){
+            creationRepertoire();
+        } else if(strcmp(operation, "suppression_repertoire\0")){
+            suppressionRepertoire();
+        } else if(strcmp(operation, "lire_fichier\0")){
+            lireFichier();
+        } else {
+            fprintf(stderr, "\n" );
+            return EXIT_FAILURE;
         }
     }
 
