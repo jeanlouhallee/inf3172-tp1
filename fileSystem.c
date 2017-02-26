6f037b961d7de60d310841f82e9df5cf0d9d1f2f
Etienne <etiennebergeron91@gmail.com>
2017-02-26 17:52:49 -0500

    Lecture du chemin absolu

--- fileSystem.c
+++ fileSystem.c
@@ -5,6 +5,8 @@
 #include <unistd.h>
 #include "fileSystem.h"
 
+#define MAX_CHEMIN 41
+
 int main(int argc, char *argv[]){
     // Fichier d'operations donne en parametre
     if(argc != 2){
@@ -21,15 +23,15 @@ int main(int argc, char *argv[]){
     // Lecture des operations
     while(fscanf(operations, "%s", operation) != EOF){        
         if(strcmp(operation, "creation_fichier\0")){
-            creationFicher();
+            creationFicher(operations);
         } else if(strcmp(operation, "suppression_fichier\0")){
-            suppressionFichier();
+            suppressionFichier(operations);
         } else if(strcmp(operation, "creation_repertoire\0")){
-            creationRepertoire();
+            creationRepertoire(operations);
         } else if(strcmp(operation, "suppression_repertoire\0")){
-            suppressionRepertoire();
+            suppressionRepertoire(operations);
         } else if(strcmp(operation, "lire_fichier\0")){
-            lireFichier();
+            lireFichier(operations);
         } else {
             fprintf(stderr, "\n" );
             return EXIT_FAILURE;
@@ -40,22 +42,65 @@ int main(int argc, char *argv[]){
     fclose(disque);
 }
 
-void creationFicher(){
+void lireCheminAbsolu(FILE *operations, char *chemin){
+    char repertoire[MAX_CHEMIN];
 
+    fscanf(operations, "/");
+    while(fscanf(operations, "%s/", repertoire)){
+        //Verifie si le repertoire existe
+
+        //Verifie si le chemin est trop long
+        if((strlen(chemin) + strlen(repertoire)) <= 40){
+            strcat(chemin, repertoire);
+            strcat(chemin, "/");
+        } else {
+            fprintf(stderr, "\n" );
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return;
 }
 
-void suppressionFichier(){
+void creationFicher(FILE *operations){
+    char chemin[MAX_CHEMIN] = "";
+    char nom[MAX_CHEMIN];
 
+    // Verifie si le disque est plein
+
+    lireCheminAbsolu(operations, chemin);
+
+    // Lecture du nom de fichier
+    fscanf(operations, "%s", nom);
+
+    // Verifie si le fichier existe deja
+
+    // Lecture du contenu du fichier
+    fscanf(operations, "%[^\n]", );
+
+    // Verifie si le fichier est trop volumineux
 }
 
-void creationRepertoire(){
+void suppressionFichier(FILE *operations){
+    char chemin[MAX_CHEMIN];
 
+    lireCheminAbsolu(operations, chemin);
 }
 
-void suppressionRepertoire(){
+void creationRepertoire(FILE *operations){
+    char chemin[MAX_CHEMIN];
 
+    lireCheminAbsolu(operations, chemin);
 }
 
-void lireFichier(){
+void suppressionRepertoire(FILE *operations){
+    char chemin[MAX_CHEMIN];
 
+    lireCheminAbsolu(operations, chemin);
+}
+
+void lireFichier(FILE *operations){
+    char chemin[MAX_CHEMIN];
+
+    lireCheminAbsolu(operations, chemin);
 }
